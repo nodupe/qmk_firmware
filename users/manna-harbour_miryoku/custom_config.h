f5ee1827516e37622090441958156830a3681168
Vinicius Provenzano <vinicius@provenzano.me>
2024-08-20 07:16:15 +0200

    first attempt to customize extra keys

--- users/manna-harbour_miryoku/custom_config.h
+++ users/manna-harbour_miryoku/custom_config.h
@@ -28,5 +28,16 @@
 #undef  RGBLIGHT_EFFECT_TWINKLE
 
 #endif
-
+#define MIRYOKU_MAPPING( \
+      K00,  K01,  K02,  K03,  K04,         K05,  K06,  K07,  K08,  K09, \
+      K10,  K11,  K12,  K13,  K14,         K15,  K16,  K17,  K18,  K19, \
+      K20,  K21,  K22,  K23,  K24,         K25,  K26,  K27,  K28,  K29, \
+      N30,  N31,  K32,  K33,  K34,         K35,  K36,  K37,  N38,  N39 \
+) \
+LAYOUT_split_3x6_3( \
+KC_TILDE,  K00,  K01,  K02,  K03,  K04,         K05,  K06,  K07,  K08,  K09,  C(KC_C), \
+KC_HYPR,  K10,  K11,  K12,  K13,  K14,         K15,  K16,  K17,  K18,  K19,  C(KC_X), \
+KC_MEH,  K20,  K21,  K22,  K23,  K24,         K25,  K26,  K27,  K28,  K29,  LALT(KC_X), \
+                      K32,  K33,  K34,         K35,  K36,  K37                                \
+)
 
